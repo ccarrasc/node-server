ff986d53e6693024ad5e70b78ef8baa3f89e7642
Cesar Carrasco <cesar@machinemode.com>
2013-07-18 22:40:28 -0400

    Added npm install to provision script

--- provision.sh
+++ provision.sh
@@ -9,3 +9,8 @@ sudo apt-get install nodejs -y
 
 # Install the Heroku Toolbelt
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Install node packages as declared in package.json
+cd /vagrant
+npm install
+
