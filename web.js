070f4e1425d37cc9eb19120c2ddd491e4ed6b794
cesar <cesar@machinemode.com>
2013-10-11 09:07:01 -0400

    Added ejs sample

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
 var express = require('express');
+var ejs = require('ejs');
 
 var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
-/*
-app.get('/', function(request, response) {
-  response.send(process.versions);
+app.engine('html', ejs.renderFile);
+
+
+app.get('/stuff', function(request, response) {
+  response.render('stuff.html', { versions: JSON.stringify(process.versions).replace(/,/g, '<br>') });
 });
-*/
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
